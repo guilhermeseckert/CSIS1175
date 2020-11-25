071a5322cef587f03678ece05f3a3d688ea75624
Guilherme Sonnemann Eckert <sonnermanneckertg@student.douglascollege.ca>
2020-11-24 18:55:35 -0800

    Add project files.

---
+++ Revenue/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Revenue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(215, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(550, 164);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sort by Name";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sort by Revenue";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(215, 293);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(550, 137);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(201, 81);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(120, 24);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "By Revenue";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(36, 81);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.radioButton1.Size = new System.Drawing.Size(124, 24);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Within Name";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(357, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 36);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(278, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Guilherme Movie Revenue";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ Revenue/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Linq;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Revenue
+{
+    public partial class Form1 : Form
+
+    {
+        int size = 16;
+        string[] movie;
+        double[] movieSales;
+        string pathMovie = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"MovieRevenue.txt");
+
+        public Form1()
+        {
+            InitializeComponent();
+            ReadintoArray();
+        }
+
+   
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReadintoArray()
+        {
+          
+      
+
+            var lines = File.ReadLines(pathMovie).ToArray();
+            string []fields;
+            movie = new string[size];
+            double output;
+            movieSales = new double[size];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+               fields = lines[i].Split('@', '}', '@', '=', '~');
+                movie[i] = fields[0].Trim();
+                double.TryParse(fields[1].Trim(), out output);
+                movieSales[i] = output;
+     
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.ForeColor = Color.Yellow;
+            listBox1.BackColor = Color.Blue;
+            SelectionSortByName(movie, movieSales);
+
+            for (int i = 0; i < Math.Max(movieSales.Length, movie.Length); i++)
+            {
+                listBox1.Items.Add(movie[i] + ">>>>>>>>" + movieSales[i].ToString("c"));
+            }
+
+        }
+
+        static void SelectionSortByName(string[] movie, double[] sale )
+        {
+            int pos = 1;
+            while (pos < movie.Length)
+            {
+                if (String.Compare(movie[pos], movie[pos - 1], StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    pos++;
+        
+                }
+                else
+                {
+                    string temp = movie[pos];
+                    double Tempnumber = sale[pos];
+                    movie[pos] = movie[pos - 1];
+                    movie[pos - 1] = temp;
+                    sale[pos] = sale[pos - 1];
+                    sale[pos - 1] = Tempnumber;
+                    if (pos > 1) pos--;
+
+                }
+            }
+
+        }
+
+
+        private void SelectionSortByRevenue()
+        {
+            double temp;
+            string tempMovie;
+            for (int j = 0; j <= movieSales.Length - 2; j++)
+            {
+                for (int i = 0; i <= movieSales.Length - 2; i++)
+                {
+                    if (movieSales[i] < movieSales[i + 1])
+                    {
+                        temp = movieSales[i + 1];
+                        movieSales[i + 1] = movieSales[i];
+                        movieSales[i] = temp;
+                        tempMovie = movie[i + 1];
+                        movie[i + 1] = movie[i];
+                        movie[i] = tempMovie;
+                    }
+                }
+            }
+
+            for (int i = 0; i < Math.Max(movieSales.Length, movie.Length); i++)
+            {
+                listBox1.Items.Add(movie[i] + ">>>>>>>>" + movieSales[i].ToString("c"));
+            }
+
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.BackColor = Color.Red;
+            listBox1.ForeColor = Color.Black;
+            SelectionSortByRevenue();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+          
+
+
+            listBox1.Items.Clear();
+
+            if (radioButton1.Checked == true)
+            {
+                string[] result = movie.Where(x => x.ToLower().Contains(textBox1.Text.Trim().ToLower())).ToArray();
+
+                foreach (string i in result)
+                {
+                    int index = Array.IndexOf(movie, i);
+                    listBox1.Items.Add(i + ">>>>>" + movieSales[index]);
+                }
+
+            }
+
+            if(radioButton2.Checked == true)
+            {
+
+                
+
+            }
+          
+
+
+
+
+
+        }
+
+        public int BinarySearchIterative(double[] inputArray, double key)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (key == inputArray[mid])
+                {
+                    return ++mid;
+                }
+                else if (key < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+
+    }
+
+}
+
+    
